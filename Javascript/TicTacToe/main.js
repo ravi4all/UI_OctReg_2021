afbad28f9850d255432b166a6c14b8b0446fbfaf
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-01-20 21:04:18 +0530

    Add files via upload

--- Javascript/TicTacToe/main.js
+++ Javascript/TicTacToe/main.js
@@ -1,7 +1,16 @@
 window.addEventListener("load", initEvents);
 
 var btns;
+var user_choice;
+var cpu_choice;
 function initEvents() {
+    user_choice = document.querySelector("#choice").value;
+    if(user_choice == 'X') {
+        cpu_choice = '0';
+    }
+    else {
+        cpu_choice = 'X';
+    }
     btns = document.querySelectorAll(".btn");
     for(var i = 0; i < btns.length; i++) {
         btns[i].addEventListener("click", userMove);
@@ -9,7 +18,8 @@ function initEvents() {
 }
 
 function userMove() {
-    this.innerHTML = 'X';
+    this.innerHTML = user_choice;
+    this.setAttribute('disabled',true);
     setTimeout(function() {
         cpuMove();
     }, 1000);
@@ -17,5 +27,13 @@ function userMove() {
 
 function cpuMove() {
     cpuPosition = parseInt(Math.random() * 9);
-    btns[cpuPosition].innerHTML = '0';
+    if(btns[cpuPosition].innerHTML == '0' || btns[cpuPosition].innerHTML == 'X') {
+        console.log("Tried..."+cpuPosition);
+        cpuMove();
+    }
+    else {
+        btns[cpuPosition].innerHTML = cpu_choice;
+        btns[cpuPosition].setAttribute('disabled',true);
+    }
+    
 }
